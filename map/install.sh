6b113c7253d712579803b18d97db4a88f2497491
Khaled Hazime <hazimekhaled01@gmail.com>
2022-03-14 19:45:56 -0300

    Restored package to the working one

---
+++ map/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get -y install ros-kinetic-map-server
+
+chmod +x scripts/*
--- save_local/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt-get -y install ros-melodic-map-server
-sudo apt-get -y install ros-melodic-base-local-planner*
-
-chmod +x scripts/*
